00bae398c2fc3a5b0fccfc84684a0d45412e7d42
sabithahabeeb <sabithahabeeb.sabi@gmail.com>
2023-11-17 13:02:13 +0530

    multer

--- Middlewares/jwtMiddleware.js
+++ Middlewares/jwtMiddleware.js
@@ -6,7 +6,7 @@ const token = req.headers['authorization'].split(" ")[1]
 try{
     const jwtResponse = jwt.verify(token,"supersecretkey12345")
     console.log(jwtResponse);
-    req.payload = jwtResponse.userId
+    req.payload = jwtResponse.userID
     next()
 }catch(err){
     res.status(401).json("Authorization failed!!! Please Login...")
--- Routes/router.js
+++ Routes/router.js
@@ -3,6 +3,7 @@ const router = new express.Router()
 const userController = require('../Controllers/userController')
 const projectController = require('../Controllers/projectController')
 const jwtMiddleware = require('../Middlewares/jwtMiddleware')
+const multerConfig = require('../Middlewares/multerMiddleware')
 // register API
 router.post('/user/register',userController.register)
 
@@ -10,7 +11,7 @@ router.post('/user/register',userController.register)
 router.post('/user/login',userController.login)
 
 // add-project
-router.post('/project/add',jwtMiddleware,projectController.addProjects)
+router.post('/project/add',jwtMiddleware,multerConfig.single('projectImage'),projectController.addProjects)
 
 // export router
 module.exports = router
